8c33611bb07f8b3b2ebc31b3f65d71c54ce2259c
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-06-25 17:30:32 -0600

    Add: load minecraft saves
    	Implementation of MinecraftSavesReader, and it's repository to
    	read the saves in the computer.

---
+++ src/logic/MinecraftSaves.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Map_Renderer.src.logic
+{
+    public class MinecraftSaves
+    {
+        #region Class Variables
+        
+        public readonly List<MinecraftSave> Saves = new List<MinecraftSave>();
+        private MinecraftSavesReader SavesReader;
+        
+        #endregion
+        
+        public MinecraftSaves()
+        {
+            SavesReader = new MinecraftSavesReader();
+
+            SavesReader.LoadSaves(Saves);
+        }
+    }
+}
--- src/ui/MinecraftMapRendererForm.cs
+++ src/ui/MinecraftMapRendererForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minecraft_Map_Renderer.src.logic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
