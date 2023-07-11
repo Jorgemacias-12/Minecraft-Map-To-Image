a0b99ddb0a8de40d68c0c12898804f452a744eb8
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-07-11 03:13:19 -0600

    ADd: ImageFormats
    	This class contains a dictionary that contains the available
    	image type files that you can export the map

---
+++ src/utils/ImageFormats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Map_Renderer.src.utils
+{
+    public class ImageFormats
+    {
+        private static Dictionary<string, ImageFormat> _Formats;
+        
+        public static Dictionary<string, ImageFormat> Formats
+        {
+            get
+            {
+                if (_Formats == null) HandleLoadFormats();
+
+                return _Formats;
+            }
+
+        }
+
+        private static void HandleLoadFormats()
+        {
+            _Formats = new Dictionary<string, ImageFormat>
+            {
+                { "bmp", ImageFormat.Bmp },
+                { "gif", ImageFormat.Gif },
+                { "jpeg", ImageFormat.Jpeg },
+                { "jpg", ImageFormat.Jpeg },
+                { "png", ImageFormat.Png }
+            };
+        }
+    }
+}
