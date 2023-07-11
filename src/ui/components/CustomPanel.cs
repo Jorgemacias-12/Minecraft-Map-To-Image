f6084855fbc95118077deb125e45c5040c66ed74
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-07-11 03:11:47 -0600

    Add: custom panel
    	This component allows to have a container that you can modify
    	the border.

---
+++ src/ui/components/CustomPanel.cs
@@ -0,0 +1,50 @@
+﻿using Minecraft_Map_Renderer.src.utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Map_Renderer.src.ui.components
+{
+    public class CustomPanel : Panel
+    {
+        private Border _Border;
+        
+
+        [Browsable(true)]
+        public Border Border;
+
+        public CustomPanel()
+        {
+            Border = new Border();
+
+            Border.AllSize = 1;
+            Border.AllColor = Color.White;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            ControlPaint.DrawBorder(e.Graphics,
+                                    ClientRectangle,
+                                    Border.LeftColor,
+                                    Border.LeftSize,
+                                    ButtonBorderStyle.Solid,
+                                    Border.TopColor,
+                                    Border.TopSize,
+                                    ButtonBorderStyle.Solid,
+                                    Border.RightColor,
+                                    Border.RightSize,
+                                    ButtonBorderStyle.Solid,
+                                    Border.BottomColor,
+                                    Border.BottomSize,
+                                    ButtonBorderStyle.Solid);
+        }
+    }
+}
