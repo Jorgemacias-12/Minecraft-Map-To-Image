395aa9d0b8ca6dd1cf58683c48c59396f839233b
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-07-23 20:58:07 -0600

    Add: new implementation of the loader
    	There's a new way to load the saves

---
+++ src/logic/MinecraftSavesLoader.cs
@@ -0,0 +1,56 @@
+﻿using Minecraft_Map_Renderer.src.utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Map_Renderer.src.logic
+{
+    public class MinecraftSavesLoader
+    {
+        public MinecraftSavesLoader()
+        {
+
+        }
+
+        public async Task LoadSaves(List<MinecraftSave> Saves, MinecraftEdition edition)
+        {
+            string[] MinecraftSavesPath;
+            MinecraftSave Save;
+
+            switch(edition)
+            {
+                case MinecraftEdition.Java:
+
+                    MinecraftSavesPath = await Task.Run(() =>
+                    {
+                        return Directory.GetDirectories(Minecraft.JAVA_SAVES_PATH);
+                    });
+                    
+                    foreach(string SavePath in MinecraftSavesPath)
+                    {
+                        Save = new MinecraftSave(Path.GetFileName(SavePath),
+                                                 SavePath);
+
+                        if (Save is null) continue;
+
+                        Saves.Add(Save);
+                    }
+
+                    break;
+
+                case MinecraftEdition.Bedrock:
+
+                    MinecraftSavesPath = await Task.Run(() =>
+                    {
+                        return Directory.GetDirectories(Minecraft.BEDROCK_SAVES_PATH);
+                    });
+
+                    // Implement this later :P
+                    break;
+            }
+        } 
+    }
+}
