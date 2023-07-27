4e085b6777a04235a0ba7edfa8d18768b9b5cb9b
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-07-27 07:26:52 -0600

    Add: MinecraftEdition, MinecraftDimension and...
    	Also added multiple path existence checks due to errors in
    	runtime.
    
    	And acess tonthe minecraft saves between the two editions of
    	minecraft.
    
    	The last is the async task to Load Minecraft Data, this only
    	includes Java Edition when it's used, but the implementation
    	has the two edition contempled.

--- src/logic/Minecraft.cs
+++ src/logic/Minecraft.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Minecraft_Map_Renderer.src.logic
 {
+    public enum MinecraftEdition
+    {
+        Java,
+        Bedrock
+    }
+
+    public enum MinecraftDimension
+    {
+        Nether = -1,
+        Overworld = 0,
+        The_End = 1
+    }
+
     public class Minecraft
     {
         private static Minecraft _Instance;
@@ -25,16 +39,18 @@ namespace Minecraft_Map_Renderer.src.logic
         public readonly static string LOCAL_APPLICATION_DATA = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         
         public readonly static string JAVA_INSTALATION_PATH = Path.Combine(APPDATA_PATH, ".minecraft");
+        public readonly static string JAVA_VERSIONS_PATH = Path.Combine(JAVA_INSTALATION_PATH, "versions");
         public readonly static string BEDROCK_INSTALATION_PATH = Path.Combine(LOCAL_APPLICATION_DATA,
                                                                               "Packages",
                                                                               "Microsoft.MinecraftUWP_8wekyb3d8bbwe");
 
-        public readonly static string MINECRAFT_JAVA_SAVES_PATH = Path.Combine(JAVA_INSTALATION_PATH, "saves");
-        public readonly static string MINECRAFT_BEDROCK_SAVES_PATH = Path.Combine(BEDROCK_INSTALATION_PATH,
+        public readonly static string JAVA_SAVES_PATH = Path.Combine(JAVA_INSTALATION_PATH, "saves");
+        public readonly static string BEDROCK_SAVES_PATH = Path.Combine(BEDROCK_INSTALATION_PATH,
                                                                                   "LocalState",
                                                                                   "games",
                                                                                   "com.mojang",
                                                                                   "minecraftWorlds");
+
 
         public bool IsMinecraftJavaInstalled
         {
@@ -56,7 +72,7 @@ namespace Minecraft_Map_Renderer.src.logic
         {
             get
             {
-                return Directory.Exists(MINECRAFT_JAVA_SAVES_PATH);
+                return Directory.Exists(JAVA_SAVES_PATH);
             }
         }
 
@@ -64,12 +80,68 @@ namespace Minecraft_Map_Renderer.src.logic
         {
             get
             {
-                return Directory.Exists(MINECRAFT_BEDROCK_SAVES_PATH);
+                return Directory.Exists(BEDROCK_SAVES_PATH);
             }
-        } 
+        }
+
+        private List<MinecraftSave> _JavaSaves;
+        private List<MinecraftSave> _BedrockSaves;
+
+        public List<MinecraftSave> JavaSaves
+        {
+            get
+            {
+                if (_JavaSaves == null) 
+                {
+                    _JavaSaves = new List<MinecraftSave>();
+                }
+
+                return _JavaSaves;
+            }
+        }
+
+        public List<MinecraftSave> BedrockSaves
+        {
+            get
+            {
+                if (_BedrockSaves == null)
+                {
+                    _BedrockSaves = new List<MinecraftSave>();
+                }
+
+                return _BedrockSaves;
+            }
+        }
 
         private Minecraft()
         {
+            
+        }
+
+        public async Task LoadMinecraftData(MinecraftEdition edition)
+        {
+            MinecraftSavesLoader _Loader = new MinecraftSavesLoader();
+
+            switch (edition)
+            {
+                case MinecraftEdition.Java:
+
+                    await _Loader.LoadSaves(JavaSaves, MinecraftEdition.Java);
+
+                    foreach(MinecraftSave save in JavaSaves)
+                    {
+                        save.Maps = await MinecraftMapsLoader.LoadMaps(save.Path, save.Version);
+                    }
+
+                    break;
+
+                case MinecraftEdition.Bedrock:
+
+                    await _Loader.LoadSaves(BedrockSaves, MinecraftEdition.Bedrock);
+                    
+                    break;
+            }
+
         }
 
     }
