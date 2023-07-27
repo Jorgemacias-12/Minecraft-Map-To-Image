602da5343ac0797ffa2bc95e58cf5433000f9684
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-07-27 08:03:15 -0600

    Add: Minecraft Save Reader class
    	This class has methods that allows to read level.dat
    	to know about the save.

---
+++ src/utils/MinecraftSaveReader.cs
@@ -0,0 +1,56 @@
+﻿using fNbt;
+using Minecraft_Map_Renderer.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Map_Renderer.src.utils
+{
+    public class MinecraftSaveReader
+    {
+
+        public static bool TryReadVersion(string SavePath, out string Version)
+        {
+            string LevelDatPath = Path.Combine(SavePath, "level.dat");
+
+            NbtFile nbtFile = new NbtFile();
+
+            nbtFile.LoadFromFile(LevelDatPath);
+
+            NbtCompound rootCompound = nbtFile.RootTag;
+
+            NbtCompound dataCompound = rootCompound.Get<NbtCompound>("Data");
+
+            if (!dataCompound.Contains("Version"))
+            {
+                Version = "Unknown";
+
+                return false;
+            }
+
+            NbtCompound versionCompound = dataCompound.Get<NbtCompound>("Version");
+
+            Version = versionCompound.Get<NbtString>("Name").StringValue;
+
+            return true;
+        }
+
+        public static bool TryReadSplashIcon(string SavePath, out Image Icon)
+        {
+            if (!Directory.Exists(SavePath))
+            {
+                Icon = Resources.grass_block;
+
+                return false;
+            }
+
+            Icon = Image.FromFile($"{SavePath}\\icon.png");
+
+            return true;
+        }
+    }
+}
