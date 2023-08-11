167c81a21345ef59f00ba3c052e67f9947aaa72e
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-08-11 01:24:23 -0600

    Rename: due to bad naming

--- src/ui/forms/MinecraftEditionSelectorForm.Designer.cs
+++ src/ui/forms/MinecraftEditionSelectorForm.Designer.cs
@@ -32,10 +32,13 @@
             this.Lbl_Title = new System.Windows.Forms.Label();
             this.Pnl_Container = new System.Windows.Forms.Panel();
             this.Tlp_Editions = new System.Windows.Forms.TableLayoutPanel();
+            this.Pnl_Exit = new System.Windows.Forms.Panel();
+            this.Btn_Exit = new System.Windows.Forms.Button();
             this.Mev_Bedrock = new Minecraft_Map_Renderer.src.ui.forms.MinecraftEditionView();
             this.Mev_Java = new Minecraft_Map_Renderer.src.ui.forms.MinecraftEditionView();
             this.Pnl_Container.SuspendLayout();
             this.Tlp_Editions.SuspendLayout();
+            this.Pnl_Exit.SuspendLayout();
             this.SuspendLayout();
             // 
             // Lbl_Title
@@ -58,7 +61,7 @@
             this.Pnl_Container.Location = new System.Drawing.Point(0, 50);
             this.Pnl_Container.Name = "Pnl_Container";
             this.Pnl_Container.Padding = new System.Windows.Forms.Padding(10);
-            this.Pnl_Container.Size = new System.Drawing.Size(766, 450);
+            this.Pnl_Container.Size = new System.Drawing.Size(766, 449);
             this.Pnl_Container.TabIndex = 1;
             // 
             // Tlp_Editions
@@ -74,27 +77,57 @@
             this.Tlp_Editions.Padding = new System.Windows.Forms.Padding(10);
             this.Tlp_Editions.RowCount = 1;
             this.Tlp_Editions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.Tlp_Editions.Size = new System.Drawing.Size(746, 430);
+            this.Tlp_Editions.Size = new System.Drawing.Size(746, 429);
             this.Tlp_Editions.TabIndex = 0;
+            // 
+            // Pnl_Exit
+            // 
+            this.Pnl_Exit.Controls.Add(this.Btn_Exit);
+            this.Pnl_Exit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Pnl_Exit.Location = new System.Drawing.Point(0, 499);
+            this.Pnl_Exit.Name = "Pnl_Exit";
+            this.Pnl_Exit.Padding = new System.Windows.Forms.Padding(10);
+            this.Pnl_Exit.Size = new System.Drawing.Size(766, 51);
+            this.Pnl_Exit.TabIndex = 1;
+            // 
+            // Btn_Exit
+            // 
+            this.Btn_Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(211)))), ((int)(((byte)(176)))));
+            this.Btn_Exit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn_Exit.FlatAppearance.BorderSize = 0;
+            this.Btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Exit.ForeColor = System.Drawing.Color.White;
+            this.Btn_Exit.Location = new System.Drawing.Point(10, 10);
+            this.Btn_Exit.Name = "Btn_Exit";
+            this.Btn_Exit.Size = new System.Drawing.Size(746, 31);
+            this.Btn_Exit.TabIndex = 0;
+            this.Btn_Exit.Text = "Salir";
+            this.Btn_Exit.UseVisualStyleBackColor = false;
+            this.Btn_Exit.Click += new System.EventHandler(this.Btn_Exit_Click);
             // 
             // Mev_Bedrock
             // 
             this.Mev_Bedrock.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.Mev_Bedrock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Mev_Bedrock.Edition = Minecraft_Map_Renderer.src.logic.MinecraftEdition.Java;
             this.Mev_Bedrock.EditionName = null;
             this.Mev_Bedrock.Image = null;
-            this.Mev_Bedrock.Location = new System.Drawing.Point(3, 3);
+            this.Mev_Bedrock.Location = new System.Drawing.Point(13, 13);
             this.Mev_Bedrock.Name = "Mev_Bedrock";
-            this.Mev_Bedrock.Size = new System.Drawing.Size(367, 419);
+            this.Mev_Bedrock.Size = new System.Drawing.Size(357, 403);
             this.Mev_Bedrock.TabIndex = 0;
             // 
             // Mev_Java
             // 
             this.Mev_Java.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.Mev_Java.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Mev_Java.Edition = Minecraft_Map_Renderer.src.logic.MinecraftEdition.Java;
             this.Mev_Java.EditionName = null;
             this.Mev_Java.Image = null;
-            this.Mev_Java.Location = new System.Drawing.Point(376, 3);
+            this.Mev_Java.Location = new System.Drawing.Point(376, 13);
             this.Mev_Java.Name = "Mev_Java";
-            this.Mev_Java.Size = new System.Drawing.Size(367, 419);
+            this.Mev_Java.Size = new System.Drawing.Size(357, 403);
             this.Mev_Java.TabIndex = 1;
             // 
             // MinecraftEditionSelectorForm
@@ -102,10 +135,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
-            this.ClientSize = new System.Drawing.Size(766, 500);
+            this.ClientSize = new System.Drawing.Size(766, 550);
             this.ControlBox = false;
             this.Controls.Add(this.Pnl_Container);
             this.Controls.Add(this.Lbl_Title);
+            this.Controls.Add(this.Pnl_Exit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -115,6 +149,7 @@
             this.Load += new System.EventHandler(this.MinecraftEditionSelectorForm_Load);
             this.Pnl_Container.ResumeLayout(false);
             this.Tlp_Editions.ResumeLayout(false);
+            this.Pnl_Exit.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -126,5 +161,7 @@
         private System.Windows.Forms.TableLayoutPanel Tlp_Editions;
         private MinecraftEditionView Mev_Bedrock;
         private MinecraftEditionView Mev_Java;
+        private System.Windows.Forms.Panel Pnl_Exit;
+        private System.Windows.Forms.Button Btn_Exit;
     }
 }
---
+++ src/ui/forms/MinecraftEditionSelectorForm.cs
@@ -0,0 +1,65 @@
+﻿using Minecraft_Map_Renderer.Properties;
+using Minecraft_Map_Renderer.src.logic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Map_Renderer.src.ui.forms
+{
+    public partial class MinecraftEditionSelectorForm : Form
+    {
+        public MinecraftEditionSelectorForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void SetUpEditions()
+        {
+            Mev_Bedrock.Image = Resources.bedrock_block;
+            Mev_Java.Image = Resources.glass_block_3d;
+
+            Mev_Bedrock.EditionName = "Bedrock";
+            Mev_Java.EditionName = "Java";
+
+            Mev_Bedrock.Edition = MinecraftEdition.Bedrock;
+            Mev_Java.Edition = MinecraftEdition.Java;
+        }
+        
+        private void VerifyMinecraftInstalations()
+        {
+            if (!Minecraft.Instance.IsMinecraftJavaInstalled)
+            {
+                // Disappear here Mev_Java
+                Mev_Java.Dispose();
+
+                // Show a modal to ask for the Java instalation path
+            }
+
+            if (!Minecraft.Instance.IsMinecraftBedrockInstalled)
+            {
+                // Disappear here Mev_Bedrock
+                Mev_Bedrock.Dispose();
+
+                // Show a modal to ask for the Bedrock instalation path
+            }
+        }
+
+        private void MinecraftEditionSelectorForm_Load(object sender, EventArgs e)
+        {
+            SetUpEditions();
+            VerifyMinecraftInstalations();
+        }
+
+        private void Btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
