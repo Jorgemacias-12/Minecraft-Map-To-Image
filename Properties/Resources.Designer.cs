0e7075199d231d90efc2757b4c8f2fea42ca227e
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-03-30 23:26:14 -0600

    remove(files): remove previous project structure

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Minecraft_Map_Renderer.Properties {
+namespace Minecraft_Map_To_Image.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Minecraft_Map_Renderer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Minecraft_Map_Renderer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Minecraft_Map_To_Image.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
