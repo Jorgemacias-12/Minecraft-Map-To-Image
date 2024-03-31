c30c87d26f91a833c6008a87941e69811d2507d0
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-03-30 23:51:45 -0600

    update(assemblyinfo)
    Change assembly title, description, company, product and copyright.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Minecraft Map Renderer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Minecraft Map Renderer")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTitle("Minecraft Map To Image")]
+[assembly: AssemblyDescription("Export Minecraft map items from a save to image format like: bmp, jpeg, png")]
+[assembly: AssemblyCompany("Jorge Macias & Eduardo Robles")]
+[assembly: AssemblyProduct("Minecraft Map To Image")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("*.*.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
