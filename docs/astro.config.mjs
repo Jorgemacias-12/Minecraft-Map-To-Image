8cc85be175efc75a7c74b340c437d27ebff22fc4
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-14 15:46:22 -0600

    update(docs): astro config
    Define i18n configuration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,26 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "NBTMap Explorer",
+      defaultLocale: "en",
+      locales: {
+        en: {
+          label: "English",
+        },
+        es: {
+          label: "Español",
+        },
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/Jorgemacias-12/Minecraft-Map-To-Image",
+        },
+      ],
+      sidebar: [],
+    }),
+  ],
 });
