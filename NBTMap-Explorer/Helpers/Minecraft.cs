b78e286074bff791fa040389e495313d67568432
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-21 16:54:32 -0600

    update: Minecraft.cs (helper)
    Add the following fields:
    
    - JAVA_INSTALLATION_PATH
    - JAVA_VERSIONS_PATH
    - BEDROCK_INSTALLATION_PATH
    - JAVA_SAVES_PATH
    - BEDROCK_SAVES_PATH
    
    This commits references to task #13

--- NBTMap-Explorer/Helpers/Minecraft.cs
+++ NBTMap-Explorer/Helpers/Minecraft.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,20 @@ namespace NBTMap_Explorer.Helpers
 
     public class Minecraft
     {
+        public readonly static string APPDATA_PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public readonly static string LOCAL_APPLICATION_DATA = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        public readonly static string JAVA_INSTALLATION_PATH = Path.Combine(APPDATA_PATH, ".minecraft");
+        public readonly static string JAVA_VERSIONS_PATH = Path.Combine(JAVA_INSTALLATION_PATH, "versions");
+        public readonly static string BEDROCK_INSTALLATION_PATH = Path.Combine(LOCAL_APPLICATION_DATA,
+                                                                              "Packages",
+                                                                              "Microsoft.MinecraftUWP_8wekyb3d8bbwe");
+
+        public readonly static string JAVA_SAVES_PATH = Path.Combine(JAVA_INSTALLATION_PATH, "saves");
+        public readonly static string BEDROCK_SAVES_PATH = Path.Combine(BEDROCK_INSTALLATION_PATH,
+                                                                                  "LocalState",
+                                                                                  "games",
+                                                                                  "com.mojang",
+                                                                                  "minecraftWorlds");
     }
 }
