8b9ff7da898a900509fea8bd8c610edf4a02afcf
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-21 17:03:29 -0600

    add: MinecraftInstallation (Service)
     Implement the following fields:
    
     - (boolean) IsMinecraftJavaInstalled
     - (boolean) IsMinecraftBedrockInstalled
     - (string) JavaEditionError
     - (string) BedrockEditionError
    
    The following methods:
    
     - (boolean) JavaEditionHasSaves
     - (boolean) BedrockEditionHasSaves
    
     This commit references to the task #13

---
+++ NBTMap-Explorer/Services/MinecraftInstallation.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using NBTMap_Explorer.Helpers;
+
+namespace NBTMap_Explorer.Services
+{
+    public static class MinecraftInstallation
+    {
+        public static bool IsMinecraftJavaInstalled => Directory.Exists(Minecraft.JAVA_INSTALLATION_PATH);
+        public static bool IsMinecraftBedrockInstalled => Directory.Exists(Minecraft.BEDROCK_INSTALLATION_PATH);
+
+        public static string JavaEditionError = string.Empty;
+        public static string BedrockEditionError = string.Empty;
+
+        public static bool JavaEditionHasSaves()
+        {
+            try
+            {
+                string[] saves = Directory.GetDirectories(Minecraft.JAVA_SAVES_PATH);
+
+                return IsMinecraftJavaInstalled && saves.Length > 0;
+            }
+            catch (Exception ex)
+            {
+                JavaEditionError = ex.Message;
+                return false;
+            }
+        }
+
+
+        public static bool BedrockEditionHasSaves()
+        {
+            try
+            {
+                string[] saves = Directory.GetDirectories(Minecraft.BEDROCK_SAVES_PATH);
+
+                return IsMinecraftBedrockInstalled && saves.Length > 0;
+            }
+            catch (Exception ex)
+            {
+                BedrockEditionError = ex.Message;
+                return false;
+            }
+        }
+
+
+    }
+}
