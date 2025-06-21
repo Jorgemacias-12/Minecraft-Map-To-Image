bdf7713a887bb52223d29ace7e33f7175f2b47f7
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-21 16:16:58 -0600

    add: Minecraft helper
    Will contain enums and the path of the installation

---
+++ NBTMap-Explorer/Helpers/Minecraft.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBTMap_Explorer.Helpers
+{
+    public enum MinecraftEdition
+    {
+        Java,
+        Bedrock
+    }
+
+    public enum MinecraftDimension
+    {
+        Overworld,
+        Nether,
+        End
+    }
+
+    public class Minecraft
+    {
+    }
+}
