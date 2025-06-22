90df2c0f0e8c4aa001971143a27d5978671cf850
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-21 21:23:53 -0600

    add: MinecraftMap.cs (Model)
    This is only a DTO

---
+++ NBTMap-Explorer/Models/MinecraftMap.cs
@@ -0,0 +1,22 @@
+﻿using NBTMap_Explorer.Helpers;
+using System.Windows.Controls;
+
+namespace NBTMap_Explorer.Models
+{
+    public class MinecraftMap
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Dimension { get; set; }
+        public byte Locked { get; set; }
+        public byte Scale { get; set; }
+        public byte TrackingPosition { get; set; }
+        public byte UnlimitedTracking { get; set; }
+        public int XCenter { get; set; }
+        public int ZCenter { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Image? Image { get; set; }
+        public byte[] Data = new byte[Minecraft.MAP_BYTE_SIZE];
+    }
+}
