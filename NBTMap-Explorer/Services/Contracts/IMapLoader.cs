68de23aa6e4636aa7488259054485b3b1d7645d4
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-21 21:33:20 -0600

    add: Interfaces (contracts)
    Create IMapLoader.cs and IWorldLoader, later to be used in Services to
    load the data from users pc.
    
    This commit is for the task #12 #6

---
+++ NBTMap-Explorer/Services/Contracts/IMapLoader.cs
@@ -0,0 +1,16 @@
+﻿using NBTMap_Explorer.Helpers;
+using NBTMap_Explorer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBTMap_Explorer.Services.Contracts
+{
+    public interface IMapLoader
+    {
+        Task<MinecraftWorld?> LoadWorldAsync(string worldPath);
+        Task<IEnumerable<MinecraftWorld>> LoadAllWorldsAsync(MinecraftEdition edition);
+    }
+}
---
+++ NBTMap-Explorer/Services/Contracts/IWorldLoader.cs
@@ -0,0 +1,9 @@
+﻿using NBTMap_Explorer.Models;
+
+namespace NBTMap_Explorer.Services.Contracts
+{
+    public interface IWorldLoader
+    {
+        Task<MinecraftMap[]> LoadMapsAsync(string worldPaths);
+    }
+}
