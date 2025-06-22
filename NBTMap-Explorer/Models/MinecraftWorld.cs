9d68e04e61d8d05be468416f66db3dc6307f719e
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-21 21:26:42 -0600

    add: MinecraftWorld (model)
    This is only a DTO

---
+++ NBTMap-Explorer/Models/MinecraftWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace NBTMap_Explorer.Models
+{
+    public class MinecraftWorld
+    {
+        public string Name { get; init; }
+        public string Path { get; init; }
+        public string Version { get; init; }
+        public Image? SplashIcon { get; init; }
+        public bool HasMaps { get; init; }
+        public List<MinecraftMap> Maps { get; init; } = [];
+    }
+}
