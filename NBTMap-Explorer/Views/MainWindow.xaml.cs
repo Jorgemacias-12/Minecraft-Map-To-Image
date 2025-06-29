0aee8c545b53cdd92f2533698d0a198482722e4d
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-29 11:08:01 -0600

    update: using SystemWindowBase

--- NBTMap-Explorer/Views/MainWindow.xaml.cs
+++ NBTMap-Explorer/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using NBTMap_Explorer.Helpers;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +15,10 @@ namespace NBTMap_Explorer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : SystemWindowBase
     {
         public MainWindow()
         {
-            InitializeComponent();
         }
     }
 }
