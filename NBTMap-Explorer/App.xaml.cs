15f83e637093f5e35b185766249c9b495926b32e
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-29 09:07:49 -0600

    update: App startup (WIP)
    Not finished just testing purposes at the moment

--- NBTMap-Explorer/App.xaml.cs
+++ NBTMap-Explorer/App.xaml.cs
@@ -1,14 +1,30 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace NBTMap_Explorer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public IServiceProvider ServiceProvider { get; private set; }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            var window = new MainWindow();
+
+            window.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            // Register your services here
+            // Example: services.Add
+
+        }
+    }
 }
