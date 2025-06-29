69dbf97f63725c7bdcfc32ba61ab12f1074b4405
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-06-29 11:18:02 -0600

    update: Services WIP

---
+++ NBTMap-Explorer/Services/JavaMapLoader.cs
@@ -0,0 +1,24 @@
+﻿using NBTMap_Explorer.Helpers;
+using NBTMap_Explorer.Models;
+using NBTMap_Explorer.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBTMap_Explorer.Services
+{
+    public class JavaMapLoader : IMapLoader
+    {
+        public Task<IEnumerable<MinecraftWorld>> LoadAllWorldsAsync(MinecraftEdition edition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MinecraftWorld?> LoadWorldAsync(string worldPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- NBTMap-Explorer/Services/JavaWorldLoader.cs
+++ NBTMap-Explorer/Services/JavaWorldLoader.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NBTMap_Explorer.Models;
+using NBTMap_Explorer.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace NBTMap_Explorer.Services
 {
-    public class JavaWorldLoader
+    public class JavaWorldLoader : IWorldLoader
     {
+        public Task<MinecraftMap[]> LoadMapsAsync(string worldPaths)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
